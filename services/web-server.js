585081568988033ec9b8809e3c67b0d393a2eba3
Eric <ericfragoso@gmail.com>
2019-04-18 22:52:34 -0300

    implementando rotas

--- services/web-server.js
+++ services/web-server.js
@@ -13,9 +13,6 @@ function initialize(){
         httpServer = http.createServer(app);
 
         app.use(morgan('combined'));
-        app.use(express.json({
-            reviver: reviveJson;
-        }));
 
         app.use('/api',router);
 
@@ -42,16 +39,6 @@ function close(){
     });
 }
 
-const iso8601RegExp = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/;
- 
-function reviveJson(key, value) {
-  // revive ISO 8601 date strings to instances of Date
-  if (typeof value === 'string' && iso8601RegExp.test(value)) {
-    return new Date(value);
-  } else {
-    return value;
-  }
-}
 
 module.exports.close = close;
 module.exports.initialize = initialize;
