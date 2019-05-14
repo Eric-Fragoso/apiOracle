6a3e75b8d29fba641179e7b73e0676138cd74efc
Eric <ericfragoso@gmail.com>
2019-05-14 18:05:35 -0300

    Ajustando Galeria de imagens qualidade

--- db_apis/controles.js
+++ db_apis/controles.js
@@ -65,11 +65,24 @@ async function importa(context) {
     binds.CULTURA = context.cultura;
     //console.log(binds);  
 
-    query = `\n \n select vp.COD_FORNECEDOR as COD_FORNECEDOR, vp.ANO as ANO, vp.MES, to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
-    vp.DATA,decode(upper(substr(vp.SAFRA,1,1)),'M','Manga','U','Uva','C','Cacau','Outra') as CULTURA,
-    vp.VARIEDADE, vp.CONTROLE as CONTROLE, vp.SAFRA, sum(vp.PESO) as VOLUME_KG                                                                                                                  
+    query = `\n select 
+      vp.COD_FORNECEDOR as COD_FORNECEDOR, 
+      vp.ANO as ANO, 
+      vp.MES, 
+      to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
+      vp.DATA,
+      decode(upper(substr(vp.SAFRA,1,1)),'M','Manga','U','Uva','C','Cacau','Outra') as CULTURA,
+      vp.VARIEDADE, 
+      vp.CONTROLE as CONTROLE, 
+      vp.SAFRA, 
+      sum(vp.PESO) as VOLUME_KG,
+      round(sum(case when(vp.PROCESSO = 1) then d.PESO else 0 end),2) as RECEPCAO,
+      round(sum(case when(vp.PROCESSO in (3.1,3.2)) then d.PESO else 0 end),2) as SELECAO,         
+      round(sum(case when(vp.PROCESSO in (4.1,4.12,4.21,4.24)) then d.PESO else 0 end),2) as EMBALAMENTO,                  
+      round(sum(case when(vp.PROCESSO = 6) then d.PESO else 0 end),2) EXPEDICAO  
+
       from mgagr.agr_bi_visaoprodutivaph_dq vp
-      where vp.PROCESSO = 1 AND vp.CONTROLE = :CONTROLE AND vp.ANO = :ANO AND vp.SAFRA = :CULTURA 
+      where vp.PROCESSO in (1, 3.1, 3.2, 4.1, 4.12, 4.21, 4.24, 6) AND vp.CONTROLE = :CONTROLE AND vp.ANO = :ANO AND vp.SAFRA = :CULTURA 
       group by
       vp.COD_FORNECEDOR,
       vp.ANO,
