8e219d374e9a96fe1765c51a83efbac7c700d7bc
Eric <ericfragoso@gmail.com>
2019-05-14 18:12:33 -0300

    Ajustando Galeria de imagens qualidade

--- db_apis/controles.js
+++ db_apis/controles.js
@@ -77,7 +77,7 @@ async function importa(context) {
       vp.SAFRA, 
       sum(vp.PESO) as VOLUME_KG                                                                                                                  
       from mgagr.agr_bi_visaoprodutivaph_dq vp
-      where vp.PROCESSO = 1 AND vp.CONTROLE = :CONTROLE AND vp.ANO = :ANO AND vp.SAFRA = :CULTURA 
+      where vp.CONTROLE = :CONTROLE AND vp.ANO = :ANO AND vp.SAFRA = :CULTURA 
       group by
       vp.COD_FORNECEDOR,
       vp.ANO,
