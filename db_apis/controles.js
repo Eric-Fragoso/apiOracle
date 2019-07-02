808f62cc3f994f949fafb712baf901e2c7e8283f
Eric <ericfragoso@gmail.com>
2019-07-02 00:03:09 -0300

    Finalizando controles produtor

--- db_apis/controles.js
+++ db_apis/controles.js
@@ -63,9 +63,7 @@ async function importa(context) {
     binds.CONTROLE = context.id;
     binds.ANO = context.ano;
     binds.CULTURA = context.cultura;
-    console.log(context.etapa);  
-    if (context.etapa == "Recepcao"){
-      query = `\n select vp.COD_FORNECEDOR as COD_FORNECEDOR, vp.ANO as ANO, vp.MES, to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
+    query = `\n select vp.COD_FORNECEDOR as COD_FORNECEDOR, vp.ANO as ANO, vp.MES, to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
       vp.DATA,decode(upper(substr(vp.SAFRA,1,1)),'M','Manga','U','Uva','C','Cacau','Outra') as CULTURA,
       vp.VARIEDADE, vp.CONTROLE as CONTROLE, vp.SAFRA, sum(vp.PESO) as VOLUME_KG                                                                                                                  
       from mgagr.agr_bi_visaoprodutivaph_dq vp
@@ -83,52 +81,7 @@ async function importa(context) {
       vp.CONTROLE,
       vp.SAFRA
       order by vp.DATA`; 
-    }else if(context.etapa == "Selecao"){
-      query = `\n select vp.COD_FORNECEDOR as COD_FORNECEDOR, vp.ANO, vp.MES, to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA, 
-      vp.DATA, decode(upper(substr(vp.SAFRA,1,1)),'M','Manga','U','Uva','C','Cacau','Outra') as CULTURA,
-      vp.VARIEDADE, vp.CONTROLE as CONTROLE, vp.SAFRA,
-      sum(case when (vp.PROCESSO = 2 and vp.MERCADO like 'M.I%' )then vp.PESO else 0 end)as VOLUME_KG_MI,
-      sum(case when (vp.PROCESSO = 2 and vp.MERCADO not like 'M.I%' )then vp.PESO else 0 end)as VOLUME_KG_ME,
-      sum(case when (vp.PROCESSO = 4 and upper(vp.MERCADO) not like '%SELE%' ) then vp.PESO else 0 end) as VOLUME_KG_REFUGO         
-                                                                                                                        
-      from mgagr.agr_bi_visaoprodutivaph_dq vp
-      where vp.PROCESSO in (2,4) AND vp.CONTROLE = :CONTROLE AND vp.ANO = :ANO AND vp.SAFRA = :CULTURA
-      group by
-            vp.ANO,
-            vp.MES,
-            to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')),
-            vp.DATA,
-            decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
-                                            ,'U','Uva'
-                                            ,'C','Cacau','Outra'),
-            vp.VARIEDADE,
-            vp.CONTROLE 
-            order by vp.DATA`;  
-    }else if(context.etapa == "Embalagem"){
-      query = `\n select vp.COD_FORNECEDOR as COD_FORNECEDOR, vp.ANO,vp.MES,to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
-      vp.DATA, decode(upper(substr(vp.SAFRA,1,1)),'M','Manga','U','Uva','C','Cacau','Outra') as CULTURA,
-      vp.VARIEDADE,vp.CONTROLE as CONTROLE, vp.SAFRA,
-      sum(case when (vp.PROCESSO = 3 and vp.MERCADO like 'M.I%' )then vp.PESO 
-               else 0 end)as VOLUME_KG_MI,
-      sum(case when (vp.PROCESSO = 3 and vp.MERCADO not like 'M.I%' )then vp.PESO 
-               else 0 end)as VOLUME_KG_ME,
-      sum(case when (vp.PROCESSO = 4 and upper(vp.MERCADO) not like '%EMB%' ) then vp.PESO 
-               else 0 end) as VOLUME_KG_REFUGO         
-                                                                                                                        
-      from mgagr.agr_bi_visaoprodutivaph_dq vp
-      where vp.PROCESSO in (3,4) AND vp.CONTROLE = :CONTROLE AND vp.ANO = :ANO AND vp.SAFRA = :CULTURA
-      group by
-      vp.ANO,
-      vp.MES,
-      to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')),
-      vp.DATA,
-      decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
-                                      ,'U','Uva'
-                                      ,'C','Cacau','Outra'),
-      vp.VARIEDADE,
-      vp.CONTROLE
-      order by vp.DATA`; 
-    }
+    
       
   }
 
