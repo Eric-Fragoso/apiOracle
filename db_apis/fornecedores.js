6c8d5774fc330fdfa41c9e287c71eae6df8e0fa9
Eric <ericfragoso@gmail.com>
2019-08-07 13:04:08 -0300

    Finalizando controles produtor

--- db_apis/fornecedores.js
+++ db_apis/fornecedores.js
@@ -66,54 +66,10 @@ async function comercialControle(context) {
     binds.CONTROLE = context.controleId;
     binds.FORNECEDOR = context.fornecedorId;
 
-    console.log("aqui foi");
+    query = `select *
+    from mgagr.agr_bi_visaocomercial_dq vc where COD_FORNECEDOR = :FORNECEDOR and CONTROLE = :CONTROLE and TIPO_LANCAMENTO = 'REALIZADO'
 
-    query = `select        
-    vc.TIPO_LANCAMENTO,
-    vc.CONTROLE,
-    vc.COD_FORNECEDOR,
-    vc.MERCADO,
-    vc.NOTA_FISCAL,
-    vc.CONTAINER,              
-    vc.CARREGAMENTO, 
-    vc.SAFRA, 
-    vc.DATA_EMBARQUE,
-    decode(upper(substr(vc.SAFRA,1,1)),'M','Manga'
-                                      ,'U','Uva'
-                                      ,'C','Cacau','Outra') as CULTURA,
-    vc.VARIEDADE,
-    vc.CAIXA,
-    vc.VLPERC_COMISSAO,
-    vc.COMISSAO_IBACEM,
-    VC.MOEDA,
-    VC.CAMBIO,
-    sum(vc.VALOR_BRUTO_CX) as VALOR_BRUTO_CX,             
-    sum(vc.OUTRAS_DESP_CX) as OUTRAS_DESP_CX,              
-    sum(VC.DESP_FRETE_CX) as DESP_FRETE_CX,
-    sum(vc.CUSTO_PH) as CUSTO_PH,
-    sum(vc.QTD_CAIXA) as QTD_CAIXA,
-    sum(vc.PESO_CX) as KG
-    from mgagr.agr_bi_visaocomercial_dq vc where COD_FORNECEDOR = :FORNECEDOR and CONTROLE = :CONTROLE
-
-    group by
-    vc.TIPO_LANCAMENTO,
-    vc.CONTROLE,
-    vc.COD_FORNECEDOR,
-    vc.MERCADO,
-    vc.NOTA_FISCAL,
-    vc.CONTAINER,
-    vc.CARREGAMENTO, 
-    vc.SAFRA,               
-    vc.DATA_EMBARQUE,
-    decode(upper(substr(vc.SAFRA,1,1)),'M','Manga'
-                                      ,'U','Uva'
-                                      ,'C','Cacau','Outra'),
-    vc.VARIEDADE,
-    vc.CAIXA,
-    vc.VLPERC_COMISSAO,
-    vc.COMISSAO_IBACEM,
-    VC.MOEDA,
-    VC.CAMBIO`;
+    `;
   }
 
   const result = await database.simpleExecute(query, binds);
