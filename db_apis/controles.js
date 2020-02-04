21414e847a36485fce9cf520f576aa5b93dfcbeb
Eric <ericfragoso@gmail.com>
2020-02-04 16:21:07 -0300

    alteração expedição carregando anos anteriores

--- db_apis/controles.js
+++ db_apis/controles.js
@@ -210,7 +210,7 @@ async function exibeexp(context) {
     vc.VARIEDADE,
     sum(vc.QTD_CAIXA) as QTD_CAIXA,
     sum(vc.PESO_CX) as KG
-from mgagr.agr_bi_visaocomercial_dq vc where vc.CONTROLE = :CONTROLE AND vp.ANO = :ANO
+from mgagr.agr_bi_visaocomercial_dq vc where vc.CONTROLE = :CONTROLE AND vc.ANO = :ANO
 group by
     vc.CONTROLE,
     vc.MERCADO,
