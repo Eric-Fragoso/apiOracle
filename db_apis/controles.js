1c14099108ae624bf19ee0c385bf44f372929f24
Eric <ericfragoso@gmail.com>
2020-02-05 17:57:41 -0300

    alteração expedição carregando anos anteriores

--- db_apis/controles.js
+++ db_apis/controles.js
@@ -214,7 +214,7 @@ async function exibeexp(context) {
     sum(vc.PESO_CX) as KG
 from mgcli.cli_dw_visaocomercial_dq vc 
 
-where vc.CONTROLE = :CONTROLE AND vc.ano_embarque = :ANO
+where vc.CONTROLE = :CONTROLE AND vc.ano_embarque = :ANO AND CULTURA = :CULT
 
 group by
     vc.CONTROLE,
